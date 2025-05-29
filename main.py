393fe476be410730dab2adcd845060bd754fffe1
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-05-29 20:35:10 +0500

    dd

---
+++ main.py
@@ -0,0 +1,12 @@
+# Masalan: n = 123 → javob: 131
+
+# def next_palindrome(n):
+#     def is_palindrome(x):
+#         return str(x) == str(x)[::-1]
+    
+#     n += 1
+#     while not is_palindrome(n):
+#         n += 1
+#     return n
+
+# print(next_palindrome(123))  # 131
