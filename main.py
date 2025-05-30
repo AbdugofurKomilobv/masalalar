21c0f6abbd2bbf35e4bc05181e9f21d6e1633576
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-05-30 20:14:26 +0500

    ff

--- main.py
+++ main.py
@@ -13,11 +13,33 @@
 
 
 
-age = int(input('yosh: '))
+# age = int(input('yosh: '))
 
-if age > 0 and 18 > age:
-    print('siz xali voyaga yetmagansiz')
-elif age >= 18 and age <= 150:
-    print('siz voyaga yetgansiz')
-else:
-    print('R.I.P')
+# if age > 0 and 18 > age:
+#     print('siz xali voyaga yetmagansiz')
+# elif age >= 18 and age <= 150:
+#     print('siz voyaga yetgansiz')
+# else:
+#     print('R.I.P')
+
+try:
+    age = int(input("Yoshingizni kiriting: "))
+
+    if age <= 0:
+        print("Yosh noto‘g‘ri kiritilgan. Siz hali tug‘ilmagansiz shekilli :)")
+    elif age < 7:
+        print("Siz go‘dak bolalarsiz – bog‘chaga qatnayapsizmi?")
+    elif 7 <= age < 12:
+        print("Siz boshlang‘ich sinf o‘quvchisisiz.")
+    elif 12 <= age < 18:
+        print("Siz o‘smir yoshidasiz – maktab yoki kollej o‘quvchisi bo‘lishingiz mumkin.")
+    elif 18 <= age < 30:
+        print("Siz voyaga yetgansiz va yoshlik davridasiz – o‘qish, ishlash yoki oilaviy hayot boshlanishi.")
+    elif 30 <= age < 60:
+        print("Siz to‘liq voyaga yetgan, hayot tajribasi bor insonsiz.")
+    elif 60 <= age <= 150:
+        print("Siz keksalar safidasiz – tajriba va donolik sizda.")
+    else:
+        print("R.I.P – bu yoshda yashaganlar juda kam. Balki noto‘g‘ri kiritgandirsiz?")
+except ValueError:
+    print("Iltimos, butun son kiriting.")
