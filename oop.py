4e5e2bb01bbd5080d7f4e77b398d20b9f5c71602
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-06-03 20:04:37 +0500

    gg

--- oop.py
+++ oop.py
@@ -10,13 +10,29 @@
 # print(car1.info())
 
 # meros olish
-class Animal:
-    def speak(self):
-        return "Nimadir tovush chiqaryapti"
+# class Animal:
+#     def speak(self):
+#         return "Nimadir tovush chiqaryapti"
 
-class Dog(Animal):
-    def speak(self):
-        return "Vov-vov!"
+# class Dog(Animal):
+#     def speak(self):
+#         return "Vov-vov!"
 
-dog = Dog()
-print(dog.speak())
+# dog = Dog()
+# print(dog.speak())
+
+
+class Account:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+acc = Account(1000)
+acc.deposit(500)
+print(acc.get_balance())
+
