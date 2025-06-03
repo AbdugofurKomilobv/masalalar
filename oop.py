98790502a5274beeff57417f50a204bed39641aa
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-06-03 20:03:56 +0500

    ff

--- oop.py
+++ oop.py
@@ -1,10 +1,22 @@
-class Car:
-    def __init__(self, model, year):
-        self.model = model
-        self.year = year
+# class Car:
+#     def __init__(self, model, year):
+#         self.model = model
+#         self.year = year
 
-    def info(self):
-        return f"{self.year} yildagi {self.model}"
+#     def info(self):
+#         return f"{self.year} yildagi {self.model}"
 
-car1 = Car("Lacetti", 2020)
-print(car1.info())
+# car1 = Car("Lacetti", 2020)
+# print(car1.info())
+
+# meros olish
+class Animal:
+    def speak(self):
+        return "Nimadir tovush chiqaryapti"
+
+class Dog(Animal):
+    def speak(self):
+        return "Vov-vov!"
+
+dog = Dog()
+print(dog.speak())
